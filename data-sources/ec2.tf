f3a7036f9c1e779f32d47a6a95941427ffddb432
Prasadraju99 <evaraprasadraju.azure@gmail.com>
2024-10-17 18:18:18 +0530

    terraform

--- data-sources/ec2.tf
+++ data-sources/ec2.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "terraform" {
-  ami                    = "data.aws_id.joindevops.id"
+  ami                    = data.aws_ami.joindevops.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-
   tags = {
     Name = "sample-server"
   }
